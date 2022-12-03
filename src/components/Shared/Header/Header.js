ce9eb1985552e2801f0bd1d69a1987cb129ac93c
developerorin2002 <developerorin2002@gmail.com>
2022-12-04 03:39:46 +0600

    responsive issue solved

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -7,23 +7,21 @@ const Header = () => {
     return (
         <div>
             <div className="container">
-                <div className="row py-3">
-                    <div className="col-lg-2">
-                        <img src={logoImg} className="w-100 img-fluid" alt="" />
-                    </div>
+                    
                     <div className="col-lg-10">
                         <Navbar bg="light" expand="lg">
                             <Container fluid>
+                                <Navbar.Brand><img src={logoImg} className="w-100 img-fluid" alt="" /></Navbar.Brand>
                                 <Navbar.Toggle aria-controls="navbarScroll" />
                                 <Navbar.Collapse id="navbarScroll">
                                     <Nav
-                                        className="mx-auto my-2 my-lg-0"
+                                        className="ms-auto my-2 my-lg-0"
                                         style={{ maxHeight: '100px' }}
                                         navbarScroll
                                     >
-                                        <Link to="/login" className="nav-link-cs">Login</Link>
-                                        <Link to="/signup" className="nav-link-cs">SignUp</Link>
-                                        <Link to="/attendence" className="nav-link-cs">Attendence</Link>
+                                        <Link to="/login" className="nav-link-cs text-center">Login</Link>
+                                        <Link to="/signup" className="nav-link-cs text-center">SignUp</Link>
+                                        <Link to="/attendence" className="nav-link-cs text-center">Attendence</Link>
 
                                     </Nav>
 
@@ -31,7 +29,7 @@ const Header = () => {
                             </Container>
                         </Navbar>
                     </div>
-                </div>
+                
             </div>
         </div>
     );
